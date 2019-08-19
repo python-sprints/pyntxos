1a144a2385a0dda9b0982507bf980d29bf5e7ca0
joy-rosie <joytolia@homail.com>
2019-08-19 17:58:58 +0100

    added getting restaurant links

--- utils/data.py
+++ utils/data.py
@@ -1,7 +1,13 @@
 import os
 import json
+from requests_html import HTMLSession
+from functools import lru_cache
+from bs4 import BeautifulSoup
 
-default_data_path = './data'
+default_data_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
+default_file_name = 'data.json'
+bilbao_turismo_base_url = 'http://www.bilbaoturismo.net'
+
 
 dummy = [
     {'name': 'Argotia', 'latitude': 43.2590929, 'longitude': -2.9244257, 'address': 'Plaza Nueva, 48005 Bilbao, Vizcaya, Spain'},
@@ -9,7 +15,56 @@ dummy = [
     ]
 
 
-def scrape(data_path=None, dummy_flag=False):
+def scrape(dummy_flag=False):
+
+    if dummy_flag:
+        data = dummy
+    else:
+        # TODO Need to change the following to have real data
+        restaurant_links = get_bilbao_turismo_restaurant_links()
+        session = get_html_session()
+        for restaurant_link in restaurant_links:
+            r = session.get(get_bilbao_turismo_link(restaurant_link))
+            soup = BeautifulSoup(r.text, 'html.parser')
+            name = soup.find('h2', itemprop='name')
+            address = soup.find('span', itemprop='address')
+            description = soup.find('span', itemprop='description')
+            data = dummy
+
+    return data
+
+
+@lru_cache(maxsize=1)
+def get_html_session():
+    return HTMLSession()
+
+
+def get_bilbao_turismo_link(end_url):
+    base_url = bilbao_turismo_base_url
+    return f'{base_url}/{end_url}'
+
+
+def get_bilbao_turismo_restaurant_links():
+
+    pintxos_finder_link = get_bilbao_turismo_link('BilbaoTurismo/en/pintxo-finder_2')
+    session = get_html_session()
+    r = session.get(pintxos_finder_link)
+    zones = [
+        link.split('zona=')[-1].replace('&tipologia=', '')
+        for link in r.html.links if 'pintxo-finder_2?zona=' in link
+    ]
+    all_zones_link = f'{pintxos_finder_link}/?zona={",".join(zones)}'
+    restaurant_links = []
+    for page in range(1, 10):
+        r = session.get(f'{all_zones_link}&pagina={page}')
+        restaurant_links.append([link for link in r.html.links if 'pintxo-finder/' in link])
+
+    restaurant_links = list(set([item for sublist in restaurant_links for item in sublist]))
+
+    return restaurant_links
+
+
+def dump(data, data_path=None):
 
     if data_path is None:
         data_path = default_data_path
@@ -17,13 +72,7 @@ def scrape(data_path=None, dummy_flag=False):
     if not os.path.exists(data_path):
         os.makedirs(data_path)
 
-    data_file_path = os.path.join(data_path, 'data.json')
-
-    if dummy_flag:
-        data = dummy
-    else:
-        # TODO Need to change the following to have real data
-        data = dummy
+    data_file_path = os.path.join(data_path, default_file_name)
 
     with open(data_file_path, 'w') as file:
         json.dump(data, file)
@@ -34,7 +83,7 @@ def load(data_path=None):
     if data_path is None:
         data_path = default_data_path
 
-    data_file_path = os.path.join(data_path, 'data.json')
+    data_file_path = os.path.join(data_path, default_file_name)
 
     with open(data_file_path, 'r') as file:
         data = json.load(file)
--- utils/tests/test_data.py
+++ utils/tests/test_data.py
@@ -2,11 +2,21 @@ import os
 import utils.data
 
 
+data_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'data')
+
+
 def test_scrape():
-    utils.data.scrape(data_path='../../data/', dummy_flag=True)
-    assert os.path.exists('../../data/data.json')
+    data = utils.data.scrape(dummy_flag=True)
+    assert data == utils.data.dummy
+
+    data = utils.data.scrape()
+
+
+def test_dump():
+    utils.data.dump(data=utils.data.scrape(dummy_flag=True), data_path=data_path)
+    assert os.path.exists(os.path.join(data_path, utils.data.default_file_name))
 
 
 def test_load():
-    data = utils.data.load(data_path='../../data/')
+    data = utils.data.load(data_path=data_path)
     assert data == utils.data.dummy
