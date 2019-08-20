bfe405415feb38977a951f62e8b52e8415506bf5
joy-rosie <joytolia@homail.com>
2019-08-20 21:03:34 +0100

    simple comments

--- utils/tests/test_scrape.py
+++ utils/tests/test_scrape.py
@@ -4,5 +4,4 @@ import utils.scrape
 def test_scrape():
     data = utils.scrape.scrape(dummy_flag=True)
     assert data == utils.scrape.dummy
-    data = utils.scrape.scrape()
-    print('blah')
+    # data = utils.scrape.scrape()
