4e6227a275da78a3e5386582c588a8e84b7eb1c9
MarcoGorelli <m.e.gorelli@gmail.com>
2019-08-21 19:56:56 +0100

    Add option to pass in number of restaurants, add tests, black autoformatting

--- utils/viz.py
+++ utils/viz.py
@@ -1,11 +1,22 @@
 import folium
 
 
-def make_and_save_map(data):
+def make_and_save_map(data, num_restaurants=None):
 
-    m = folium.Map(location=[43.2590929, -2.9244257], zoom_start=20)
+    m = folium.Map(location=[43.2590929, -2.9244257])  # , zoom_start=20)
 
     tooltip = "Click me!"
+
+    if num_restaurants:
+        if not isinstance(num_restaurants, int):
+            raise ValueError("`num_restaurants` must be integer or None!")
+
+    if isinstance(num_restaurants, int):
+        if num_restaurants < 1:
+            raise ValueError("`num_restaurants` must be at least 1!")
+
+    if num_restaurants:
+        data = data[:num_restaurants]
 
     for i in data:
         folium.Marker(
@@ -24,5 +35,11 @@ def make_and_save_map(data):
             weight=3,
             colormap=["green", "red"],
         ).add_to(m)
+    m.fit_bounds(
+        [
+            [min([i["latitude"] for i in data]), min([i["longitude"] for i in data])],
+            [max([i["latitude"] for i in data]), max([i["longitude"] for i in data])],
+        ]
+    )
 
     return m
